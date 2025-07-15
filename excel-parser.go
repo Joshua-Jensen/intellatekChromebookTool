7c7ebc9a4aa6d8c543c6ca2ae623777d7b8354f4
josh <joshjj3@gmail.com>
2025-07-15 13:36:39 -0600

    working version with small changes

--- excel-parser.go
+++ excel-parser.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -23,8 +25,10 @@ type roomItem struct {
 }
 
 type chromebookItem struct {
-	sn       string
+	// sn       string
 	assetTag string
+	comments string
+	slotNum  string
 }
 
 func main() {
@@ -38,42 +42,53 @@ func main() {
 	env = <-envChan
 
 	//create a new file
-	fmt.Println("creating new file")
-	file := excelize.NewFile()
-	var path string = `"`+ env.path + "/" + env.fileName + ".xlsx" + `"`
+	var path string = env.path + "/" + env.fileName + ".xlsx"
+	var file *excelize.File
+	_, err := os.Stat(path)
+	if err == nil {
+		file, err = excelize.OpenFile(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		fmt.Println("creating new file")
+		file = excelize.NewFile()
+	} else {
+		fmt.Println("err ocurred", err)
+	}
+
 	createNewRoomSheet(file, path)
 	println("all done")
 	// forcing git update
 }
 
-
-
-
-
 // this function sets up the env variables
 func setupEnv() envVariables {
+	scanner := bufio.NewScanner(os.Stdin)
 	var env envVariables
 	// var sheetStr string
 	fmt.Println("enter file path")
-	fmt.Scanln(&env.path)
-	env.path = `"`+ env.path + `"`
+	// fmt.Scanln(&env.path)
+	if scanner.Scan() {
+		env.path = scanner.Text()
+	}
 	re := regexp.MustCompile(`\\`)
 	env.path = re.ReplaceAllString(env.path, "/")
 	fmt.Println("enter file name")
-	fmt.Scanln(&env.fileName)
+	// fmt.Scanln(&env.fileName)
+	if scanner.Scan() {
+		env.fileName = scanner.Text()
+	}
 	// fmt.Println("enter sheet names (comma separated)")
 	// fmt.Scanln(&sheetStr)
 	// env.sheetNames = strings.Split(sheetStr, ",")
 	fmt.Println("env variables set")
 	fmt.Println("clear terminal")
+
 	time.Sleep(1 * time.Second)
 	// clearTerminal()
 	return env
 }
-
-
-
-
 
 // this function clears the terminal
 // unused while testing
@@ -87,10 +102,6 @@ func clearTerminal() {
 	}
 	_ = cmd.Run()
 }
-
-
-
-
 
 func createNewRoomSheet(file *excelize.File, path string) {
 	var loop bool = true
@@ -107,22 +118,19 @@ func createNewRoomSheet(file *excelize.File, path string) {
 				fmt.Println("Error creating sheet:", err)
 			}
 			createRoomContents(file, newRoom, path)
-		}else{
+		} else {
 			loop = false
 		}
 	}
 }
-
-
-
-
-
 
 // this function takes int he file and the env,path and creates a new sheet for each room with its contents
 func createRoomContents(file *excelize.File, newRoom string, path string) {
 	// scan in each chromebook by room
 
 	//collect the data on the chromebooks
+	scanner := bufio.NewScanner(os.Stdin)
+
 	var loop bool = true
 	var roomList roomItem
 	roomList.roomNumber = newRoom
@@ -130,22 +138,47 @@ func createRoomContents(file *excelize.File, newRoom string, path string) {
 		var escapeString string = "exit"
 		// var newRoomString string = "done"
 		var newChromebook chromebookItem
-		fmt.Println("enter chromebook serial number")
-		fmt.Scanln(&newChromebook.sn)
-		if newChromebook.sn != escapeString {
-			fmt.Println("entered chromebook serial number: ", newChromebook.sn)
-			fmt.Println("enter chromebook asset tag")
-			fmt.Scanln(&newChromebook.assetTag)
+		// 	fmt.Println("enter chromebook serial number")
+		// 	fmt.Scanln(&newChromebook.sn)
+		// 	if newChromebook.sn != escapeString {
+		// 		fmt.Println("entered chromebook serial number: ", newChromebook.sn)
+		// 		fmt.Println("enter chromebook asset tag")
+		// 		fmt.Scanln(&newChromebook.assetTag)
+		// 		fmt.Println("entered chromebook asset tag: ", newChromebook.assetTag)
+		// 		roomList.roomContents = append(roomList.roomContents, newChromebook)
+		// 	} else {
+		// 		loop = false
+		// 	}
+		// }
+
+		fmt.Println("enter chromebook asset tag")
+		// fmt.Scanln(&newChromebook.assetTag)
+		if scanner.Scan() {
+			newChromebook.assetTag = scanner.Text()
+		}
+
+		if newChromebook.assetTag != escapeString {
+
 			fmt.Println("entered chromebook asset tag: ", newChromebook.assetTag)
+			fmt.Println("enter comments")
+			// fmt.Scanln(&newChromebook.comments)
+			if scanner.Scan() {
+				newChromebook.comments = scanner.Text()
+			}
+			fmt.Println("enter in the slot number")
+			// fmt.Scanln(&newChromebook.slotNum)
+			if scanner.Scan() {
+				newChromebook.slotNum = scanner.Text()
+			}
 			roomList.roomContents = append(roomList.roomContents, newChromebook)
 		} else {
 			loop = false
 		}
 	}
 	// Write room data to the sheet
-	rowIndex := 2 // Start writing from the eighth row
+	rowIndex := 2 // Start writing from the second row
 	for _, chromebook := range roomList.roomContents {
-		rowData := []interface{}{"", chromebook.sn, chromebook.assetTag, "", "", "", "", "", "", roomList.roomNumber, "", "", "", "", ""}
+		rowData := []interface{}{"", chromebook.assetTag, chromebook.comments, chromebook.slotNum, "", "", "", "", roomList.roomNumber, "", "", "", "", ""}
 		err := file.SetSheetRow(roomList.roomNumber, fmt.Sprintf("A%d", rowIndex), &rowData)
 		if err != nil {
 			fmt.Println("Error writing to sheet:", err)
@@ -159,19 +192,18 @@ func createRoomContents(file *excelize.File, newRoom string, path string) {
 
 }
 
-
-
-
-
-
-
-
 func saveFile(file *excelize.File, path string) {
 	// Save the file to the specified path
-	err := file.SaveAs(path)
-	if err != nil {
-		fmt.Println("Error saving file:", err)
-		return
+	var loop = true
+	for loop {
+		err := file.SaveAs(path)
+		if err != nil {
+			fmt.Println("Error saving file:", err)
+			time.Sleep(5 * time.Second)
+		} else if err == nil {
+
+			loop = false
+		}
 	}
 	fmt.Println("File saved successfully at", path)
 }
